e9274dc68b10a0116013b909626ddbfa11dd3649
Richard Arthur <richardarthur228@gmail.com>
2022-05-11 09:03:23 +0000

    updated models

--- backend/core/models.py
+++ backend/core/models.py
@@ -398,7 +398,7 @@ class Reference(models.Model):
     name = models.CharField(max_length=50)
     document_type = models.ManyToManyField(
         DocumentType, related_name="reference_document_type")
-    last_increment = models.IntegerField()
+    last_increment = models.IntegerField(default=0, blank=True, null=True)
 
     def __str__(self):
         return self.name
