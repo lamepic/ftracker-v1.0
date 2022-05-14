d052fdc71defc42f5937336a896e318b9e7ce7fc
Richard Arthur <richardarthur228@gmail.com>
2022-05-14 23:27:00 +0000

    updated email.py

--- backend/core/email.py
+++ backend/core/email.py
@@ -1,5 +1,20 @@
-from djoser import email
+from django.contrib.auth.tokens import default_token_generator
+from templated_mail.mail import BaseEmailMessage
+from django.conf import settings as django_settings
+from djoser import utils
+from djoser.conf import settings
 
 
-class ActivationEmail(email.ActivationEmail):
-    template_name = 'email/activation.html'
+class PasswordResetEmail(BaseEmailMessage):
+    template_name = "email/password_reset.html"
+
+    def get_context_data(self):
+        # PasswordResetEmail can be deleted
+        context = super().get_context_data()
+
+        user = context.get("user")
+        context["uid"] = utils.encode_uid(user.pk)
+        context["token"] = default_token_generator.make_token(user)
+        context["url"] = settings.PASSWORD_RESET_CONFIRM_URL.format(**context)
+        context["domain"] = django_settings.WEB_URL
+        return context
