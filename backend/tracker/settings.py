b924757d04a5acd9cf9e7ac4b492821b85f24204
Richard Arthur <richardarthur228@gmail.com>
2022-05-14 23:37:21 +0000

    updated settings.py

--- backend/tracker/settings.py
+++ backend/tracker/settings.py
@@ -159,7 +159,7 @@ REST_FRAMEWORK = {
 
 # DJOSER SETTINGS
 DJOSER = {
-    'PASSWORD_RESET_CONFIRM_URL': '#/password/reset/confirm/{uid}/{token}',
+    'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
     'USERNAME_RESET_CONFIRM_URL': '#/username/reset/confirm/{uid}/{token}',
     'ACTIVATION_URL': '#/activate/{uid}/{token}',
     'SEND_ACTIVATION_EMAIL': True,
