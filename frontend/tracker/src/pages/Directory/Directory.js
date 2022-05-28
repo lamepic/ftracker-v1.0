ff08c3f8862033b68de36cee99546dfb988d0a84
Richard Arthur <richardarthur228@gmail.com>
2022-05-28 21:00:04 +0000

    updated directory

--- frontend/tracker/src/pages/Directory/Directory.js
+++ frontend/tracker/src/pages/Directory/Directory.js
@@ -229,6 +229,7 @@ function Directory() {
                       size="large"
                       shape="square"
                       block={true}
+                      key={idx}
                     />
                   );
                 })}
