cb61932416573eea4b4d42f3a49450787396a164
Richard Arthur <richardarthur228@gmail.com>
2022-05-29 12:40:54 +0000

    updated models.py

--- backend/core/models.py
+++ backend/core/models.py
@@ -429,24 +429,3 @@ def expire_date_handler(sender, instance, created, **kwargs):
 
         task = PeriodicTask.objects.create(crontab=schedule, name='document_'+str(
             secret_id), task='core.tasks.expire_document', args=json.dumps((instance.id,)))
-
-
-# @receiver(models.signals.pre_save, sender=DocumentFile)
-# def auto_delete_file_on_change(sender, instance, **kwargs):
-#     """
-#     Deletes old file from filesystem
-#     when corresponding `MediaFile` object is updated
-#     with new file.
-#     """
-#     if not instance.pk:
-#         return False
-
-#     try:
-#         old_file = DocumentFile.objects.get(pk=instance.pk).doc_file
-#     except DocumentFile.DoesNotExist:
-#         return False
-
-#     new_file = instance.doc_file
-#     if not old_file == new_file:
-#         if os.path.isfile(old_file.path):
-#             os.remove(old_file.path)
