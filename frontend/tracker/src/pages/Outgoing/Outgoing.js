fe98eb80a041107b4e53a28493d9c01b19851cb6
Richard Arthur <richardarthur228@gmail.com>
2022-06-11 15:42:34 +0000

    updated outgoing.js

--- frontend/tracker/src/pages/Outgoing/Outgoing.js
+++ frontend/tracker/src/pages/Outgoing/Outgoing.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { fetchOutgoing } from "../../http/document";
 import useFetchData from "../../hooks/useFetchData";
 import Page from "../../components/Layout/Page";
+import { useStateValue } from "../../store/StateProvider";
+import { notification } from "antd";
 
 function Outgoing() {
-  const { loading, data } = useFetchData(fetchOutgoing);
+  const [data, setData] = useState([]);
+  const [store, dispatch] = useStateValue();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchData();
+  }, [store.socketSignal]);
+
+  const fetchData = async () => {
+    try {
+      const res = await fetchOutgoing(store.token);
+      const data = res.data;
+      setData(data);
+    } catch (e) {
+      notification.error({
+        message: "Error",
+        description: e.response.data.detail,
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return <Page type="outgoing" data={data} loading={loading} />;
 }
