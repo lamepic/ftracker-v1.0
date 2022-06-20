78872764d00d462bf158bd662b3183e00f064e99
Richard Arthur <richardarthur228@gmail.com>
2022-06-20 09:41:25 +0000

    updated settings.py

--- backend/tracker/settings.py
+++ backend/tracker/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'watson.middleware.SearchContextMiddleware',
+    'watson.middleware.SearchContextMiddleware',
 ]
 
 ROOT_URLCONF = 'tracker.urls'
